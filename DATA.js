c6e94323f14d1626d56cfff4da6bd0ef43658079
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-31 10:19:28 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "68be4d89-4570-4320-a0e7-a1053a644b0c",
+    "home_feed": [
+        {
+            "id": "412f9f54-202d-4830-8fc8-3232ccbeb92b",
+            "artist_name": "which",
+            "home_carousel": [
+                {
+                    "id": "60a2d5a0-700b-44ad-b5a9-8494e57c1a36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png"
+                },
+                {
+                    "id": "49773155-609e-479c-ae72-8e832337eea9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                },
+                {
+                    "id": "23747443-88ff-4dae-8786-260e20fb3222",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/121.png"
+                },
+                {
+                    "id": "1d863b37-d650-49ce-9b0a-3e0d43f82c12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "3ac5ebe7-f1c0-4d3a-961c-af3c17d3b960",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                }
+            ],
+            "art_caption": "song",
+            "art_description": "Hour allow international easy claim. Animal high close expert."
+        },
+        {
+            "id": "6ff2d823-84f2-4104-ad95-506d573ee414",
+            "artist_name": "together",
+            "home_carousel": [
+                {
+                    "id": "71e45c59-4b30-4b27-8fad-15c16cd74524",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png"
+                },
+                {
+                    "id": "f3249977-0b46-4701-ac9b-c8a0b8b05b7a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "0f6c7b3b-87d1-48e4-af3e-52014e530c90",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "1882c121-b368-4ca2-b6fe-e7b53a1e1951",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "dac52d45-c13b-4f37-aa55-5b0585f325e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png"
+                }
+            ],
+            "art_caption": "thank",
+            "art_description": "Western seven daughter firm support."
+        },
+        {
+            "id": "db02c764-f707-4329-b988-8544a05fdf22",
+            "artist_name": "religious",
+            "home_carousel": [
+                {
+                    "id": "47399530-9f22-4b86-a504-e077313d3a77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png"
+                },
+                {
+                    "id": "443f4dfe-20d2-4487-ac7d-a80723c615f5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png"
+                },
+                {
+                    "id": "2108b8fe-b5af-4f7a-a6d1-877b19558b20",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "290c5fd3-f286-4091-ba22-bbb9203ed8d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "394e1008-cc71-4563-a25b-271454304d5b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                }
+            ],
+            "art_caption": "see",
+            "art_description": "Voice cut process similar high once. Probably put result political win oil head."
+        },
+        {
+            "id": "5fdf6f93-6568-41b1-9ea2-36a710b83a6c",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "c0ef6787-c881-46c1-8c6b-d46b7063a39a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png"
+                },
+                {
+                    "id": "e5c08157-5124-4063-b014-2b32a4177545",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "0e68aade-ba63-4fac-af2b-95b668e3353e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png"
+                },
+                {
+                    "id": "4614b1b0-0c26-48f5-855c-78d411aa8a2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "99034d77-afc6-4dcf-ac94-39171bddf99e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                }
+            ],
+            "art_caption": "dark",
+            "art_description": "Movement seem so now begin me. Others hotel whether pattern although woman bar form."
+        },
+        {
+            "id": "c9633d8b-7afd-47ef-9a5f-8728e29a3da2",
+            "artist_name": "training",
+            "home_carousel": [
+                {
+                    "id": "32054fd3-6671-4f4a-be46-2aaa6b247aa2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/324.png"
+                },
+                {
+                    "id": "026748c4-5047-4c4f-a6ce-d00e224369c7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "ddca1436-11a6-4ac8-969d-eb8ed4dddf06",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/75.png"
+                },
+                {
+                    "id": "f270ac6a-1775-4a24-b484-59e67c594e07",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "238d5edf-70cb-4c08-af77-1404c7c6c78e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png"
+                }
+            ],
+            "art_caption": "section",
+            "art_description": "Majority information list coach suffer nation check. Particular control hot suggest. Player PM well bar choice ground."
+        }
+    ]
+}
